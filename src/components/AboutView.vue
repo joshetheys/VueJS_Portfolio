128285ccfa9fea9f386f8d4a42d209b1de146ef7
Joshé Theys <102968868+joshetheys@users.noreply.github.com>
2022-07-22 22:31:23 +0200

    Update

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="about" id="about">
-    <h1 class="display-2 text-center">About</h1>
+    <h1 class="display-2 text-center" style="color: #6c63ff">About</h1>
     <div class="container">
         <div class="row">
             <div class="col-xl-4 col-md-12">
-                <img class="img-fluid" src="https://i.postimg.cc/QNQnhpBV/Joshe-Jade-1.jpg" alt="My Image" style="height: 100%; ">
+                <img class="h-100 img-fluid" src="https://i.postimg.cc/QNQnhpBV/Joshe-Jade-1.jpg" alt="My Image" style="height: 100%; ">
             </div>
             <div class="col-xl-8 col-md-12">
                 <h2>
@@ -14,39 +14,18 @@
                     Aspiring  Developer
                 </h5>
                 <p class="text-capitalize">
-                    I am currently completing a course in Web
-                    Development and Programming. I am a creative, talented and motivated individual who will add value
-                    to
-                    the workplace. I have a passion for exploring creative, modern solutions to everyday problems and I
-                    believe a career as a developer allows me to explore these solutions practically.
+                   I'm Joshe Jade Theys, an aspiring junior developer. I am currently completing a course in Web
+                    Development and Programming. I am a very creative person with a passion for the arts. You can find
+                    me writing poetry over the weekends. As a result of my creative mind, I am able to solve problems in
+                    innovative ways. I am a capable leader and I undertook a leadership course at Stellenbosch
+                    University. As part of the curriculum, I learned how to be a leader who is respectful of diversity
+                    in the workplace and one who can find the best way to address problems. I believe that my creativity
+                    will improve my coding and feed my passion for web design. In High School, I undertook after school
+                    coding classes which triggered my passion.
                 </p>
-                <div class="row">
-                    <div class="col-2">
-                        <div>Birthday</div>
-                        <div>Phone</div>
-                        <div>Email</div>
-                        <div>Location</div>
-                        <div>Language</div>
-                        <div>Hobbies</div>
-                    </div>
-                    <div class="col-1">
-                        <div> : </div>
-                        <div> : </div>
-                        <div> : </div>
-                        <div> : </div>
-                        <div> : </div>
-                        <div> : </div>
-                    </div>
-                    <div class="col-4">
-                        <div>23 July 2001</div>
-                        <div>074 485 0721</div>
-                        <div>JosheTheys@gmail.com</div>
-                        <div>Cape Town</div>
-                        <div>English & Afrikaans</div>
-                        <div>Reading, watching Series</div>
-                    </div>
+               
 
-                </div>
+                
 
                 <div class="row">
                     <!-- <div class="d-grid gap-5 d-md-block"> -->
@@ -54,7 +33,7 @@
                         <button class="w-100 btn rounded-pill" style="background-color: #6c63ff; color: white" type="button">Download CV</button>
                     </div>
                     <div class="col-6">
-                        <button class="w-100 btn rounded-pill" style="background-color: #6c63ff; color: white" type="button">GitHub</button>
+                        <button class="w-100 btn rounded-pill" style="background-color: #6c63ff; color: white" type="button"> <a href="https://github.com/joshetheys">GitHub</a> </button>
                     </div>
 
 
@@ -110,12 +89,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-4">
-                Design Thinking
+             <div class="col-4">
+                Communication
                 <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 100%; background-color: #6c63ff" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
+                    <div class="progress-bar" role="progressbar" style="width: 85%; background-color: #6c63ff" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
                   </div>
             </div>
+            
             <div class="col-4">
                 Resilence
                 <div class="progress">
@@ -123,23 +103,24 @@
                   </div>
             </div>
             <div class="col-4">
-                Public Speaking
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 90%; background-color: #6c63ff" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
-                  </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-4">
                 Empathy
                 <div class="progress">
                     <div class="progress-bar" role="progressbar" style="width: 80%; background-color: #6c63ff" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
                   </div>
             </div>
+            
+        </div>
+        <!-- <div class="row">
             <div class="col-4">
-                Communication
+                Public Speaking
                 <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 85%; background-color: #6c63ff" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
+                    <div class="progress-bar" role="progressbar" style="width: 90%; background-color: #6c63ff" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+                  </div>
+            </div>
+           <div class="col-4">
+                Design Thinking
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 100%; background-color: #6c63ff" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
                   </div>
             </div>
             <div class="col-4">
@@ -148,7 +129,7 @@
                     <div class="progress-bar" role="progressbar" style="width: 90%; background-color: #6c63ff" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
                   </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
